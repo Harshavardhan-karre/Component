dc2cc6c455ebe8297a6ab3c6b0b2c2aed714645e
Harshavardhan-karre <harshavardhan.karre@zemosolabs.com>
2024-09-18 17:43:34 +0530

    Feat -Adding React Files for Sonar

---
+++ myap/src/App.tsx
@@ -0,0 +1,9 @@
+import { Button } from "@mui/material";
+import React from "react";
+import Buttons from "./Buttons.tsx";
+// import Homepage from './pages/Homepage.tsx';
+export default function App() {
+  return <div>
+    <Buttons/>
+  </div>;
+}
