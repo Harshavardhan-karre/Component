ca021aaf2496d116b671bb6470e39726a345e829
Harshavardhan-karre <harshavardhan.karre@zemosolabs.com>
2024-09-19 18:16:39 +0530

    Feat- Removing Unused Tests

--- myap/src/App.tsx
+++ myap/src/App.tsx
@@ -1,9 +1,10 @@
 import { Button } from "@mui/material";
 import React from "react";
-import Buttons from "./Buttons.tsx";
+import Pract from "./Pract.tsx";
 // import Homepage from './pages/Homepage.tsx';
 export default function App() {
   return <div>
-    <Buttons/>
+    <Pract/>
+    <i>learn react</i>
   </div>;
 }
---
+++ myap/src/Pract.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function Pract() {
+  return (
+    <div>
+      <h1>Hello This Is done From Sonar</h1>
+    </div>
+  )
+}
