2a19fe06783410ebae00233ddc796a4ce34789b6
Harshavardhan-karre <harshavardhan.karre@zemosolabs.com>
2024-09-20 15:05:45 +0530

    Feat - Adding test Files

--- myap/src/App.tsx
+++ myap/src/App.tsx
@@ -1,10 +1,8 @@
 import { Button } from "@mui/material";
 import React from "react";
-import Pract from "./Pract.tsx";
-// import Homepage from './pages/Homepage.tsx';
+import Hello from "./Hello.tsx";
 export default function App() {
   return <div>
-    <Pract/>
-    <i>learn react</i>
+    <Hello name={", World!"}/>
   </div>;
 }
