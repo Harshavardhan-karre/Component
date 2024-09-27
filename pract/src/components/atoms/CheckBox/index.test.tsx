08421b56653ac5f6f9db0e3e79c220f686119eb9
Harshavardhan-karre <harshavardhan.karre@zemosolabs.com>
2024-09-27 11:48:31 +0530

    Fix- Updated Package Issues

--- pract/src/components/atoms/CheckBox/index.test.tsx
+++ pract/src/components/atoms/CheckBox/index.test.tsx
@@ -1,23 +1,23 @@
-import React from 'react';
-import { render, screen, fireEvent } from '@testing-library/react';
-import CheckBox from './index'; 
-
-test('check for initial checkbox appearance on screen', () => {
+import React from "react";
+import { render, screen, fireEvent } from "@testing-library/react";
+import CheckBox from "./index";
+import "@testing-library/jest-dom";
+test("check for initial checkbox appearance on screen", () => {
   render(<CheckBox checked={true} onChange={() => null} />);
-  
-  const checkstatus = screen.getByRole('checkbox');
-  
+
+  const checkstatus = screen.getByRole("checkbox");
+
   expect(checkstatus).toBeInTheDocument();
-  expect(checkstatus).toBeChecked(); 
+  expect(checkstatus).toBeChecked();
 });
 
-test('checking for response from checkbox', () => {
+test("checking for response from checkbox", () => {
   const handleChange = jest.fn();
-  render(<CheckBox checked={false} onChange={handleChange} />); 
-  
-  const button = screen.getByRole('checkbox');
+  render(<CheckBox checked={false} onChange={handleChange} />);
+
+  const button = screen.getByRole("checkbox");
 
   fireEvent.click(button);
-  
+
   expect(handleChange).toHaveBeenCalledTimes(1);
 });
